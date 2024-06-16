69bc7280c8b2ded3cc56df4bdd29f336be327806
torrvick <torrvick@gmail.com>
2024-06-16 23:12:34 +0300

    Removed unused imports in Controls

--- mynotes/src/components/Controls/NewNote.jsx
+++ mynotes/src/components/Controls/NewNote.jsx
@@ -2,8 +2,6 @@ import IconButton from '@mui/joy/IconButton';
 import AddIcon from '@mui/icons-material/MapsUgcOutlined';
 import React, { useContext } from 'react';
 import AppContext from 'contexts/AppContext';
-import NoteDialog from '../Dialogs/NoteDialog';
-import Modal from '@mui/joy/Modal';
 
 function NewNote() {
 	const { openModal } = useContext(AppContext);
@@ -12,7 +10,6 @@ function NewNote() {
 			<IconButton onClick={() => openModal()} color="success" size="lg">
 				<AddIcon sx={{ fontSize: 32 }} />
 			</IconButton>
-			{/* <NoteDialog /> */}
 		</>
 	);
 }
